9e0962d020b5640acc9d420e1ec52ce3975dcb9e
lukieoo <pawkrzysciak@gmail.com>
2020-06-20 20:00:34 +0200

    RxJava inject

--- app/src/main/java/com/anioncode/gamevideodagger/MainActivity.kt
+++ app/src/main/java/com/anioncode/gamevideodagger/MainActivity.kt
@@ -2,11 +2,15 @@ package com.anioncode.gamevideodagger
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.anioncode.gamevideodagger.model.tmpUsersItem
 import com.anioncode.gamevideodagger.ui.VideoViewModel
 import com.anioncode.gamevideodagger.viewmodels.ViewModelProviderFactory
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +28,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        viewModel = ViewModelProviders.of(this, providerFactory).get(VideoViewModel::class.java)
+        //viewModel = ViewModelProviders.of(this, providerFactory).get(VideoViewModel::class.java)
+        viewModel = ViewModelProvider(this, providerFactory).get(VideoViewModel::class.java)
+
+        // With ViewModelFactory
+      //  val viewModel = ViewModelProvider(this, providerFactory).get(VideoViewModel::class.java)
+
+        viewModel.authenticateWithId(3);
+        subscribeObservers()
+    }
+
+
+    private fun subscribeObservers() {
+       // if (::viewModel.isInitialized){
+            viewModel.observeUser()!!.observe(this, object : Observer<tmpUsersItem?> {
+                override fun onChanged(t: tmpUsersItem?) {
+                    if (t != null) {
+                        text.text="${t.username}"
+                    }
+                }
+            })
+     //   }
+
+
     }
 }
--- app/src/main/java/com/anioncode/gamevideodagger/ui/VideoViewModel.kt
+++ app/src/main/java/com/anioncode/gamevideodagger/ui/VideoViewModel.kt
@@ -1,56 +1,43 @@
 package com.anioncode.gamevideodagger.ui
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
-import com.anioncode.gamevideodagger.model.tmpUsers
 import com.anioncode.gamevideodagger.model.tmpUsersItem
 import com.anioncode.gamevideodagger.network.AuthApi
-import io.reactivex.Observer
-import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class VideoViewModel : ViewModel {
 
     private val authApi: AuthApi
-    @Inject constructor( authApis: AuthApi){
+
+    private val authUser: MediatorLiveData<tmpUsersItem> = MediatorLiveData<tmpUsersItem>()
+
+    @Inject constructor( authApis: AuthApi) {
 
         authApi = authApis
-
-        if(authApi==null){
-
-            Log.d(
-                "ViewModel_Anioncode",
-                "VideoViewModel: viewmodel is not working..."
-            )
-        }else{
-            authApi.getUsers(1).toObservable().subscribeOn(Schedulers.io())
-                .subscribe( object : Observer<tmpUsersItem>{
-                    override fun onComplete() {
-                    }
-
-                    override fun onSubscribe(d: Disposable) {
-                    }
-
-                    override fun onNext(t: tmpUsersItem) {
-                        Log.d(
-                            "ViewModel_Anioncode",
-                            "tmpUsersItem:$t"
-                        )
-                    }
-
-                    override fun onError(e: Throwable) {
-                        Log.d(
-                            "ViewModel_Anioncode",
-                            "VideoViewModel:$e"
-                        )
-                    }
-                })
-            Log.d(
-                "ViewModel_Anioncode",
-                "VideoViewModel: viewmodel is working..."
-            )
-        }
     }
+
+    fun authenticateWithId(userId: Int) {
+        val source: LiveData<tmpUsersItem> =  LiveDataReactiveStreams.fromPublisher(
+            authApi.getUsers(userId)
+                .subscribeOn(Schedulers.io())
+        )
+        authUser.addSource<tmpUsersItem>(source, object : androidx.lifecycle.Observer<tmpUsersItem?> {
+
+            override fun onChanged(t: tmpUsersItem?) {
+                authUser.setValue(t)
+                authUser.removeSource(source)
+            }
+        })
+    }
+
+    fun observeUser(): LiveData<tmpUsersItem?>? {
+        return authUser
+    }
+
+
 
 }
